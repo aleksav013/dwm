a496431497080487a9e9c5abfa6c3a9636e81aef
Aleksa Vučković <aleksav013@gmail.com>
2022-05-01 17:12:37 +0200

    swallow patch

--- config.def.h
+++ config.def.h
@@ -17,6 +17,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Inconsolata Nerd Font Mono:Style=Bold:pixelsize=16:antialias=true:autohint=true" };
@@ -40,10 +41,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      	instance    title       tags mask     isfloating   monitor */
-	{ "ViberPC",    NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "discord",  	NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "KeePassXC",	NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class       instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "ViberPC",   NULL,     NULL,           1 << 0,    0,          0,          -1,        -1 },
+	{ "discord",   NULL,     NULL,           1 << 0,    0,          0,          -1,        -1 },
+	{ "KeePassXC", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
