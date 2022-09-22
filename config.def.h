81276a05cf57d221ace6316d46b7356427e299c8
Aleksa Vuckovic <aleksav013@gmail.com>
2022-09-23 00:06:40 +0200

    bochs

--- config.def.h
+++ config.def.h
@@ -47,6 +47,7 @@ static const Rule rules[] = {
 	{ "ViberPC",   NULL,     NULL,           1 << 0,    0,          0,          -1,        -1 },
 	{ "discord",   NULL,     NULL,           1 << 0,    0,          0,          -1,        -1 },
 	{ "KeePassXC", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+//	{ "Bochs",     NULL,     NULL,           0,         0,          0,          -1,        -1 },
 	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
